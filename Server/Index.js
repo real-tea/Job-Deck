b6c46ded0aa2eea02aa437d0d759c564894f7d4b
Akash <akash.0711a@gmail.com>
2022-04-22 19:24:06 +0530

    changed port

--- Server/Index.js
+++ Server/Index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
@@ -31,6 +31,6 @@ app.use("/api/applicant", applicantRoutes);
 app.use("/api/recruiter", recruiterRoutes);
 
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("The server is up and running at port 5000");
 })
