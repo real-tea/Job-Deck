2ac96d51f98fc4f4df48b9d1d8205e61e01fbc21
Akash <akash.0711a@gmail.com>
2022-04-22 19:39:38 +0530

    procfile

--- Server/Index.js
+++ Server/Index.js
@@ -17,7 +17,6 @@ app.use(cors());
 app.use(express.json());
 
 // DATABASE CONNECTION
-const DB = process.env.DATABASE;
 mongoose.connect("mongodb+srv://Jon-deck:jaypeehacks@job-deck.dhnlj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("Database connected.");
 }).catch((err) => {
@@ -30,8 +29,8 @@ mongoose.connect("mongodb+srv://Jon-deck:jaypeehacks@job-deck.dhnlj.mongodb.net/
 app.use("/api/applicant", applicantRoutes);
 app.use("/api/recruiter", recruiterRoutes);
 
-const port = process.env.PORT || 5000
+// const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-    console.log("The server is up and running at port 5000");
-})
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
